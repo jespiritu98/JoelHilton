e612dd7936d6abdd1dff9f9b689ba9eda2a79895
jespiritu98 <jespiritu98@gmail.com>
2022-02-02 20:45:51 -0700

    Update, Delete

--- JoelHilton/Controllers/HomeController.cs
+++ JoelHilton/Controllers/HomeController.cs
@@ -33,20 +33,60 @@ namespace JoelHilton.Controllers
        
         public IActionResult MovieForm(MovieResponse mr)
         {
-                
-            daContext.Add(mr);
-            daContext.SaveChanges();
-            return View("Confirmation", mr);
+            if (ModelState.IsValid)
+            {
+                daContext.Add(mr);
+                daContext.SaveChanges();
+                return View("Confirmation", mr);
+            }
+            else // If Invalid
+            {
+                ViewBag.Categories = daContext.Categories.ToList();
+                return View(mr);
+            }
+          
+ 
                 
         }
         public IActionResult MovieList()
         {
-            var movie = daContext.Responses
+            var movies = daContext.Responses
                 .Include(x => x.Category)
                 .OrderBy(x => x.Title)
                 .ToList();
 
-            return View(movie);
+            return View(movies);
+        }
+        [HttpGet]
+        public IActionResult Edit(int applicationid)
+        {
+            ViewBag.Categories = daContext.Categories.ToList();
+
+            var application = daContext.Responses.Single(x => x.ApplicationId == applicationid);
+
+            return View("MovieForm", application);
+        }
+        [HttpPost]
+        public IActionResult Edit (MovieResponse update)
+        {
+            daContext.Update(update);
+            daContext.SaveChanges();
+
+            return RedirectToAction("MovieList");
+        }
+        [HttpGet]
+        public IActionResult Delete(int applicationid)
+        {
+            var application = daContext.Responses.Single(x => x.ApplicationId == applicationid);
+
+            return View(application);
+        }
+        [HttpPost]
+        public IActionResult Delete(MovieResponse mr)
+        {
+            daContext.Responses.Remove(mr);
+            daContext.SaveChanges();
+            return RedirectToAction("MovieList");
         }
         public IActionResult MyPodcasts()
         {
--- JoelHilton/Models/MovieResponse.cs
+++ JoelHilton/Models/MovieResponse.cs
@@ -13,17 +13,19 @@ namespace JoelHilton.Models
         [Required]
         public int ApplicationId { get; set; }
        
-    
+        [Required(ErrorMessage = "Can't watch a movie without knowing the title...")]
         public string Title { get; set; }
-       
+        [Required(ErrorMessage ="I want to know when it was made!")]
         public string Year { get; set; }
-        
+        [Required(ErrorMessage ="Who directed it?")]
         public string Director { get; set; }
-  
+        [Required(ErrorMessage ="Is it a good enough rating for my wife to let me watch it?")]
         public string Rating { get; set; }
+        
         public bool Edited { get; set; }
+      
         public string LentTo { get; set; }
-     
+        [MaxLength(25)]
         public string Notes { get; set; }
 
         //Build Foreign Key Relationship
